a6a1f7b42ad064c56da6a8140720fd1ee0ffa956
Anton Kristiono <antonkristiono47@gmail.com>
2022-10-15 17:43:05 +0700

    last

--- src/components/layouts/landing.tsx
+++ src/components/layouts/landing.tsx
@@ -1,9 +1,11 @@
 import WithSubnavigation from "../nav";
+import CaptionCarousel from "../carousel"
 
 export default function LandingLayout({ children }) {
   return (
     <>
       <WithSubnavigation />
+      <CaptionCarousel />
       <main>{children}</main>
     </>
   );
