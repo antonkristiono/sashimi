adcbb2ab7e344bfa0ba6da2b0f398887af27d5cf
Anton Kristiono <antonkristiono47@gmail.com>
2022-10-24 19:10:57 +0700

    aadd

--- pages/event/index.tsx
+++ pages/event/index.tsx
@@ -1,17 +1,62 @@
+/* import { eventQuery } from '../../lib/queries'
+ * import { getClient, overlayDrafts } from '../../lib/sanity.server'
+ * import { usePreviewSubscription } from '../../lib/sanity' */
+import { createClient } from "next-sanity";
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+} from "react";
 
-import { eventQuery } from '../../lib/queries'
-import { getClient, overlayDrafts } from '../../lib/sanity.server'
-import { usePreviewSubscription } from '../../lib/sanity'
-
-export default function Event({ allEvents }) {
-  return <>aa </>;
+export default function Event(events: any) {
+  return (
+    <>
+      <ul>
+        {events.events.map(
+          (event: {
+            _id: Key | null | undefined;
+            name:
+              | string
+              | number
+              | boolean
+              | ReactElement<any, string | JSXElementConstructor<any>>
+              | ReactFragment
+              | ReactPortal
+              | null
+              | undefined;
+          }) => (
+            <li key={event._id}>{event?.eventName}</li>
+          )
+        )}
+      </ul>
+    </>
+  );
 }
 
-export async function getStaticProps({ preview = false }) {
-    const allEvents = overlayDrafts(await getClient(preview).fetch(eventQuery))
-    return {
-      props: { allEvents, preview },
-      // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
-      revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
-    }
-  }
+const client = createClient({
+  projectId: "87su1btw",
+  dataset: "production",
+  apiVersion: "2021-10-21",
+  useCdn: false,
+});
+
+export async function getServerSideProps() {
+  const events = await client.fetch(`*[_type == "event"]`);
+
+  return {
+    props: {
+      events,
+    },
+  };
+}
+
+/* export async function getStaticProps({ preview = false }) {
+ *     const allEvents = overlayDrafts(await getClient(preview).fetch(eventQuery))
+ *     return {
+ *       props: { allEvents, preview },
+ *       // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
+ *       revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
+ *     }
+ *   } */
